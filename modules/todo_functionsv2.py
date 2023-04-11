50a361e54697842eee02fbc7ab1aa83e458231f6
Dami <dami.fajinmi@hotmail.com>
2023-04-11 12:42:49 +0100

    Implemented 'add new todo items' feature to web app

--- modules/todo_functionsv2.py
+++ modules/todo_functionsv2.py
@@ -1,4 +1,8 @@
-def get_todos(filepath='files/todos.txt'):
+FILEPATH = '/Users/damifajinmi' \
+           '/PMC_PROJECTS/files/todos.txt'
+
+
+def get_todos(filepath=FILEPATH):
     """"" Reads a text file and returns a list
     of to-do items.
     """
@@ -7,7 +11,7 @@ def get_todos(filepath='files/todos.txt'):
         return todos_local
 
 
-def write_todos(todos_arg, filepath='files/todos.txt'):
+def write_todos(todos_arg, filepath=FILEPATH):
     """
     :param todos_arg: Writes the new list of to-do items
      into the text file
@@ -20,4 +24,5 @@ def write_todos(todos_arg, filepath='files/todos.txt'):
 
 if __name__ == '__main__':
     print('Hello')
+    print(get_todos())
 
--- web_app.py
+++ web_app.py
@@ -1,8 +1,13 @@
 import streamlit as st
 from modules import todo_functionsv2 as functions
 
-
 todos = functions.get_todos()
+
+
+def add_todo():
+    todo_to_add = st.session_state['new_todo'] + '\n'
+    todos.append(todo_to_add)
+    functions.write_todos(todos)
 
 
 st.title('My Todo App')
@@ -10,7 +15,8 @@ st.subheader('This is my todo app')
 st.write('This app aims to increase your productivity')
 
 for i, todo in enumerate(todos):
-    st.checkbox(todo, key=f'existing_todo{i}')
+    st.checkbox(todo, key=i)
 
-st.text_input(label='', placeholder='Add a new todo...')
+st.text_input(label='test', placeholder='Add a new todo...',
+              on_change=add_todo, key='new_todo')
 
